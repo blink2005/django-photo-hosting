85e80059108fa725f33ee9302a65863fce1baefb
blink2005 <smirnowyt@gmail.com>
2024-08-06 15:42:03 +0300

    first commit

---
+++ search_user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Information(models.Model):
+    fio = models.TextField()
+    country = models.TextField()
+    address = models.TextField()
+    telephone = models.TextField()
+    description = models.TextField()
+    photo = models.TextField()
